2bc34884a1330776d963b86d93850452bdd2a4ef
mokokom <mokokebeloke@gmail.com>
2020-01-20 16:33:42 +0100

    add git ignore file which contain config.js file with apiKey variable

--- components/fonts.js
+++ components/fonts.js
@@ -1,5 +1,8 @@
 import Template from "./template.js";
 import EventHandler from "./eventHandler.js";
+import { config } from "../config.js";
+
+console.log(config.apiKey);
 
 class Fonts {
 	constructor() {
@@ -10,17 +13,21 @@ class Fonts {
 
 	getGFonts = async () => {
 		const jsonFonts = await fetch(
-			"https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=AIzaSyAjfp0YTzzzGDEWaGuXoN4imwRA4bTSwrM "
+			`https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=${config.apiKey}`
 		);
-		const gFonts = await jsonFonts.json();
-		for (let i = 0; i < 10; i++) {
-			this.state.fontsList.push(gFonts.items[i]);
+		try {
+			const gFonts = await jsonFonts.json();
+			for (let i = 0; i < 10; i++) {
+				this.state.fontsList.push(gFonts.items[i]);
+			}
+			this.createFontContainer(this.state.fontsList);
+		} catch (error) {
+			console.log(error);
 		}
-		this.createFontContainer(this.state.fontsList);
 	};
 
 	createFontContainer = fontsList => {
-		console.log(fontsList);
+		/* 		console.log(fontsList); */
 		let gridList = document.querySelector(".grid-list");
 		/* gridList.innerHTML = ""; */
 		fontsList.forEach(fonts => {
